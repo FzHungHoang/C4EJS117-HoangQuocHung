3c70122b3392a16c24b0bd2bcab81d85a20691bd
Fzsukiyo <fzsukiyo@gmail.com>
2021-06-07 00:27:30 +0700

    Mindx

--- Buoi3/indexB3.js
+++ Buoi3/indexB3.js
@@ -49,16 +49,17 @@ let item, quantity;
             console.log(...shop);
             alert('Done');
         }
-        else if(x == 'D'){
-            quantity = Number(prompt("Enter the position"));
-            shop.splice(quantity, 1);
-            console.log(...shop);
-            alert('Done');
-        }
+        
         else if(x == 'U'){
             quantity = Number(prompt('Enter the position'));
             item = prompt('Enter the name of the new item');
             shop[quantity] = item;
+            console.log(...shop);
+            alert('Done');
+        }
+        else if(x == 'D'){
+            quantity = Number(prompt("Enter the position"));
+            shop.splice(quantity, 1);
             console.log(...shop);
             alert('Done');
         }
